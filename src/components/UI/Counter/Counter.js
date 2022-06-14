ad1175712a8f58619d604b37f03a8916e72c0c03
顾北清 <1653777235@qq.com>
2022-06-14 17:35:02 +0800

    complete p57_import_FontAwesome

--- src/components/UI/Counter/Counter.js
+++ src/components/UI/Counter/Counter.js
@@ -1,5 +1,20 @@
 import React from 'react'
 import classes from './Counter.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+
+// 引入FontAwesome
+//      安装依赖
+//           npm i --save @fortawesome/fontawesome-svg-core
+//           npm i --save @fortawesome/free-solid-svg-icons
+//           npm i --save @fortawesome/free-regular-svg-icons
+//           npm i --save @fortawesome/react-fontawesome@latest
+//      引入组件
+//           import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//      引入图标
+//          import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//      使用组件
+//           <FontAwesomeIcon icon={faPlus}/>
 
 // 计数器组件
 const Counter = (props) => {
@@ -8,7 +23,7 @@ const Counter = (props) => {
             {
                 (props.amount && props.amount !== 0) ? (
                     <React.Fragment>
-                        <button className={classes.Sub}><span>-</span></button>
+                        <button className={classes.Sub}><FontAwesomeIcon icon={faMinus}/></button>
                         <span className={classes.count}>{props.amount}</span>
                     </React.Fragment>
 
@@ -17,7 +32,7 @@ const Counter = (props) => {
             
             
             <button className={classes.Add}>
-                <span>+</span>
+                <FontAwesomeIcon icon={faPlus}/>
             </button>
         </div>
      );
