4eaa81bbf2680c008b4bebe495cfceb1814418cb
顾北清 <1653777235@qq.com>
2022-06-15 14:10:02 +0800

    complete p67_complete_cart_bar

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,12 +11,18 @@ const Cart = () => {
         <div className={classes.Cart}>
             <div className={classes.Icon}>
                 <img src={iconImg} />
-                <span className={classes.TotalAmount}>{ctx.totalAmount}</span>
+                {ctx.totalAmount === 0 ? null : 
+                    <span className={classes.TotalAmount}>
+                        {ctx.totalAmount}
+                    </span>
+                }
             </div>
+             {ctx.totalAmount === 0 ? 
+                <p className={classes.NoMeal}>未选购商品</p>:
+                <p className={classes.Price}>{ctx.totalPrice}</p>
+             }
 
-            <p className={classes.Price}>{ctx.totalPrice}</p>
-
-            <button className={classes.Button}>去结算</button>
+            <button className={`${classes.Button} ${ctx.totalAmount === 0 ? classes.Disabled : ''}`}>去结算</button>
         </div>
      );
 }
